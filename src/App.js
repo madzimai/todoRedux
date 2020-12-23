2c733ab05c0f38e5e2a344b1a46d0cf93b65398c
Sam <sam.takunda@gmail.com>
2020-12-23 10:27:05 +0200

    React Redux todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-
 function App() {
+  const [todo, setTodo] = useState("")
+  const todosSelector = state => state.app.todos//.sort((a, b) => (a.order > b.order) ? 1 : -1);
+  const todos = useSelector(todosSelector);
+  console.log({ todos });
+  const addTodoAction = todo => {
+    return {
+      type: "ADD_TODO",
+      payload: {
+        id: Date.now(),
+        task: todo,
+        order: todos.length
+      }
+    }
+  }
+
+  const upAction = order => {
+    return {
+      type: "MOVE_UP",
+      payload: {
+        order
+      }
+    }
+  }
+  const downAction = order => {
+    return {
+      type: "MOVE_DOWN",
+      payload: {
+        order
+      }
+    }
+  }
+
+  function updateTodoAction(id, task, order) {
+    return {
+      type: "EDIT_TODO",
+      payload: {
+        id: id,
+        task: task,
+        order: todos.length - 1
+      }
+    }
+  }
+  const dispatch = useDispatch()
+  function saveText() {
+    if (todo.length < 1) { }
+    dispatch(addTodoAction(todo))
+    setTodo("")
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      saveText()
+    }
+  }
+  const onDelete = (id) => {
+    dispatch(
+      {
+        type: 'DELETE_TODO',
+        payload: {
+          id: id,
+          order: todos.length - 1
+        }
+      }
+    )
+  }
+
+  const onEdit = (id, task, order) => {
+    dispatch(updateTodoAction(id, task, order))
+  }
+  const moveUp = (order) => {
+    dispatch(upAction(order))
+  }
+  const moveDown = (order) => {
+    dispatch(downAction(order))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo</h1>
+      <div className="input"  >
+        <input
+          className="new-todo"
+          type="text"
+          value={todo}
+          onChange={e => setTodo(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <Todo onEdit={onEdit} todo={todo}
+              moveUp={moveUp} moveDown={moveDown} numberOfTodos={todos.length} onDelete={onDelete} />
+
+          </li>
+        ))
+        }
+      </div>
     </div>
   );
 }
-
+function Todo({ onEdit, todo, moveDown, moveUp, numberOfTodos, onDelete }) {
+  const [editing, setEditing] = useState(false)
+  const [text, setText] = useState(todo.task, numberOfTodos)
+  if (editing === true) {
+    return (
+      <span>
+        <input
+          value={text}
+          onChange={e => setText(e.target.value)}
+          onKeyPress={event => {
+            if (event.code !== "Enter") {
+              return
+            }
+            onEdit(todo.id, text)
+            setEditing(false)
+          }}
+        />
+      </span>
+    )
+  }
+  return (
+    <span>
+      {todo.task}
+      &nbsp;&nbsp;
+      <button type="button"
+        className="btn btn-edit"
+        onClick={() => setEditing(true)}
+      >
+        edit
+      </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      {todo.order !== 0 && (
+        <button type="button" className="arrow"
+          onClick={() => moveUp(todo.order)}>
+          <i className="fa long-arrow-up"></i> up
+        </button>
+      )}
+     &nbsp;&nbsp;&nbsp;&nbsp;
+      {numberOfTodos !== todo.order + 1 && (
+        <button type="button" className="arrow"
+          onClick={() => moveDown(todo.order)}>
+          <i className="fa long-arrow-up"></i>
+       down
+        </button>
+      )}
+      <button type="button" className="btn btn-delete" onClick={() => onDelete(todo.id)}>delete</button>
+    </span>
+  )
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,101 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      task: 'playing',
+      order: 0
+    },
+    {
+      id: 2,
+      task: 'eating',
+      order: 1
+    }
+  ]
+}
+const todosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      const updatedTodos = [
+        ...state.todos, action.payload
+      ]
+      return {
+        ...state,
+        todos: updatedTodos
+      }
+    case 'EDIT_TODO':
+      const todos = [...state.todos]
+      const indexOfTodo = todos.findIndex(todo => todo.id === action.payload.id)
+      todos[indexOfTodo] = {
+        id: action.payload.id,
+        task: action.payload.task,
+        order: action.payload.order
+      }
+      return {
+        ...state,
+        todos
+      }
+    case 'MOVE_UP':
+      const orders = [...state.todos]
+      // todos = todos.sort((a, b) => orders.indexOf(a.order) - orders.indexOf(b.order));
+      // console.log(todos);
+      const indexOfMove1 = orders.findIndex(todo => todo.order === action.payload.order)
+      const indexOfMove2 = orders.findIndex(todo => todo.order === action.payload.order - 1)
+      orders[indexOfMove2] = {
+        ...orders[indexOfMove2],
+        order: action.payload.order
+      }
+      orders[indexOfMove1] = {
+        ...orders[indexOfMove1],
+        order: action.payload.order - 1
+      }
+      return {
+        ...state,
+        todos: orders.sort((a, b) => (a.order > b.order) ? 1 : -1)
+      }
+    case 'MOVE_DOWN':
+      const downs = [...state.todos]
+      const indexOfDown1 = downs.findIndex(todo => todo.order === action.payload.order)
+      const indexOfDown2 = downs.findIndex(todo => todo.order === action.payload.order + 1)
+      downs[indexOfDown2] = {
+        ...downs[indexOfDown2],
+        order: action.payload.order
+      }
+      downs[indexOfDown1] = {
+        ...downs[indexOfDown1],
+        order: action.payload.order + 1
+      }
+      //console.log(downs);
+      return {
+        ...state,
+        todos: downs.sort((a, b) => (a.order > b.order) ? 1 : -1)
+      }
 
+    case 'DELETE_TODO':
+      const updatedTodo = [...state.todos].filter(todo => todo.id !== action.payload.id)
+      const upDated = updatedTodo.map((todo, index) => ({ ...todo, order: index }))
+      console.log(upDated)
+
+      return {
+        ...state,
+        todos: updatedTodo.map((todo, index) => ({ ...todo, order: index }))
+      }
+    default:
+      return state
+  }
+}
+const reducer =
+{
+  app: todosReducer
+}
+const store = configureStore({ reducer })
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
